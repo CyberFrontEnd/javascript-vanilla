78699f867a47c25fb3b297622c2245d49f2159f2
CyberFrontEnd <mail.techcd.ru>
2024-04-25 18:03:11 +0400

    New Flipper number

--- dist/js/age.js
+++ dist/js/age.js
@@ -20,7 +20,7 @@
 //   const month = currentDate.getMonth() - birthdayDate.getMonth(); // тут мы вычитаем текущий месяц от месяца рождения
 //
 //
-//   // если месяц меньше 0 || если месяц равен 0 && в обоих случи ях меньше 0 или месяц равен 0 или день меньше чем мы планируем
+//   // если месяц меньше 0 ||(или) если месяц равен 0 &&(и) день рождения больше существующей дать
 //   // age-- что бы уменьшить возраст на 1
 //   if (month < 0 || (month === 0 && currentDate.getDate() < birthdayDate.getDate()) ) {
 //     age--
@@ -68,61 +68,63 @@
 // bthE1.addEventListener('click', calculateAge);
 
 
+// const btn = document.getElementById('btn');
+// const input = document.getElementById('birthday');
+// const result = document.getElementById('result');
+//
+// function calculate() {
+//   const inputValue = input.value;
+//   if (inputValue === '') {
+//     alert("Введи данные")
+//   } else {
+//     let age = ageCalc(inputValue);
+//    result.innerText = `Your age is ${age} ${age < 1 ? "years" : "year"} old`;
+//   }
+// }
+//
+// function ageCalc(inputValue) {
+//   const now = new Date();
+//   const birthday = new Date(inputValue);
+//   let currentDay = now.getFullYear() - birthday.getFullYear();
+//   const currentMonth = now.getMonth() - birthday.getMonth();
+//
+//   if (currentMonth < 0 || (currentMonth === 0 && now.getDate() < birthday.getDate())) {
+//     currentDay --;
+//   }
+//
+//   return currentDay;
+// }
+//
+// btn.addEventListener('click', calculate)
+
+
 const btn = document.getElementById('btn');
-const input = document.getElementById('birthday');
+const dateValue = document.getElementById('birthday');
 const result = document.getElementById('result');
 
+
 function calculate() {
-  const inputValue = input.value;
-  if (inputValue === '') {
-    alert("Введи данные")
+  const value = dateValue.value;
+  if (value === "") {
+    alert("Поле")
   } else {
-    let age = ageCalc(inputValue);
-   result.innerText = `Your age is ${age} ${age < 1 ? "years" : "year"} old`;
+    let agesFun = ageCal(value);
+    result.innerText = `Ваш возраст ${agesFun} ${agesFun ? "Года" : "Год"}`
   }
 }
 
-function ageCalc(inputValue) {
-  const now = new Date();
-  const birthday = new Date(inputValue);
-  let currentDay = now.getFullYear() - birthday.getFullYear();
-  const currentMonth = now.getMonth() - birthday.getMonth();
+function ageCal(value) {
+  const date = new Date();
+  const dateDay = new Date(value);
+  let totalYear = date.getFullYear() - dateDay.getFullYear();
+  let totalMonth = date.getMonth() - dateDay.getMonth();
 
-  if (currentMonth < 0 || (currentMonth === 0 && now.getDate() < birthday.getDate())) {
-    currentDay --;
+  if (totalMonth < 0 || (totalMonth === 0 && date.getDate() < dateDay.getDate())) {
+    totalYear--;
   }
 
-  return currentDay;
+  return totalYear;
 }
 
-btn.addEventListener('click', calculate)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+btn.addEventListener('click', calculate);
