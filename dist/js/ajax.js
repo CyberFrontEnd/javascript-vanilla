a167251ed7e80830620bc0c757b6d9c49bc95d4b
CyberFrontEnd <mail.techcd.ru>
2024-05-10 18:31:51 +0400

    New files

--- dist/js/ajax.js
+++ dist/js/ajax.js
@@ -1,18 +1,24 @@
-const xhr = new XMLHttpRequest();
-console.log(xhr);
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', () => {
+  getDate();
+});
 
-xhr.open('GET', './api/sample.txt');
-xhr.onreadystatechange = function () {
-  if (xhr.readyState === 4 && xhr.status === 200) {
-    const text  = document.createElement('p');
-    text.textContent = xhr.responseText;
-    document.body.appendChild(text)
-  } else {
-    console.log({
-      status: xhr.status,
-      text: xhr.statusText,
-      state: xhr.readyState
-    });
+function getDate() {
+  const xhr = new XMLHttpRequest();
+
+  xhr.open('GET', './api/sample.txt');
+  xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4 && xhr.status === 200) {
+      const text = document.createElement('p');
+      text.textContent = xhr.responseText;
+      document.body.appendChild(text)
+    } else {
+      console.log({
+        status: xhr.status,
+        text: xhr.statusText,
+        state: xhr.readyState
+      });
+    }
   }
+  xhr.send();
 }
-xhr.send();
