1cd3e3a0537ea79a123c5e164b90475f14d992ac
CyberFrontEnd <avoelike@gmail.com>
2024-05-22 14:16:40 +0400

    Start video 22,05,2024

--- dist/js/video.js
+++ dist/js/video.js
@@ -1,19 +1,20 @@
+// Start Initializetion
 const btn = document.querySelector(".switch-btn");
 const video = document.querySelector(".video-container");
 
+//
 btn.addEventListener("click", function () {
   if (!btn.classList.contains("slide")) {
     btn.classList.add("slide");
     video.pause();
   } else {
-    btn.classList.remove("slide");
+    btn.classList.remove("slide")
     video.play();
   }
-});
+})
 
-// preloader
+// При загрузке страницы применяется класс hide-preloader к .preloader
 const preloader = document.querySelector(".preloader");
-
 window.addEventListener("load", function () {
   preloader.classList.add("hide-preloader");
-});
+})
