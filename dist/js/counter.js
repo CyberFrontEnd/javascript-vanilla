dd4919c739ce90ce61594e4dac72b72f673775fb
CyberFrontEnd <avoelike@gmail.com>
2024-05-25 12:27:43 +0400

    Start counter 25,05,2024

--- dist/js/counter.js
+++ dist/js/counter.js
@@ -1,16 +1,18 @@
-// set inital value to zero
 let count = 0;
-// select value and buttons
+
 const value = document.querySelector("#value");
 const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
   btn.addEventListener("click", function (e) {
     const styles = e.currentTarget.classList;
+
     if (styles.contains("decrease")) {
       count--;
+
     } else if (styles.contains("increase")) {
       count++;
+
     } else {
       count = 0;
     }
@@ -18,12 +20,15 @@ btns.forEach(function (btn) {
     if (count > 0) {
       value.style.color = "green";
     }
+
     if (count < 0) {
       value.style.color = "red";
     }
+
     if (count === 0) {
       value.style.color = "#222";
     }
+
     value.textContent = count;
   });
 });
